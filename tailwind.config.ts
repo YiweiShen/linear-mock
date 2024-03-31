d6cb43237d86c4f2ad15707c4c505aace49ed5d6
Yiwei Shen <YiWeiShen@users.noreply.github.com>
2024-03-31 11:57:57 -0400

    Update tailwind.config.ts with new theme settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,68 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      sans: [
+        'SF Pro Display',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Open Sans',
+        'Helvetica Neue',
+        'sans-serif'
+      ]
     },
-  },
-  plugins: [],
-};
-export default config;
+    fontSize: {
+      xs: '0.8rem',
+      sm: '1.4rem',
+      md: '1.6rem',
+      lg: ['2.2rem', '1.2'],
+      '5xl': ['8rem', '1']
+    },
+    colors: {
+      background: '#000212',
+      white: '#fff',
+      'white-a08': 'rgba(255, 255, 255, 0.08)',
+      gray: '#858699',
+      'gray-dark': '#1f1f1f',
+      primary: '#5e6ad2',
+      'primary-light': '#7b8af7',
+      'primary-text': '#b4bcd0'
+    },
+    spacing: {
+      0: '0',
+      1: '0.4rem',
+      2: '0.8rem',
+      3: '1.2rem',
+      4: '1.6rem',
+      5: '2.0rem',
+      6: '2.4rem',
+      8: '3.2rem',
+      10: '4.0rem',
+      12: '4.8rem',
+      'navigation-height': 'var(--navigation-height)'
+    },
+    backgroundImage: {
+      primary: '#5e6ad2',
+      'hero-gradient':
+        'radial-gradient(ellipse 50% 80% at 20% 40%,rgba(93,52,221,0.1),transparent), radial-gradient(ellipse 50% 80% at 80% 50%,rgba(120,119,198,0.15),transparent)',
+      'hero-glow':
+        'conic-gradient(from 230.29deg at 51.63% 52.16%, rgb(36, 0, 255) 0deg, rgb(0, 135, 255) 67.5deg, rgb(108, 39, 157) 198.75deg, rgb(24, 38, 163) 251.25deg, rgb(54, 103, 196) 301.88deg, rgb(105, 30, 255) 360deg)'
+    },
+    boxShadow: {
+      primary: 'rgb(80 65 200 / 50%) 0px 0px 0px'
+    }
+  }
+}
+
+export default config
