bc869d6bf22a00befec48df34d2337b0954530d4
Yiwei Shen <YiWeiShen@users.noreply.github.com>
2024-04-06 21:16:18 -0400

    Add recruitment message in client side console

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { ConsoleMessage } from '@/components/console'
 import { Header } from '@/components/header'
 import { Footer } from '@/components/footer'
 
@@ -15,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <ConsoleMessage />
       <head>
         <meta
           name="viewport"
