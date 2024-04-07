43da77adf363784812a2c3da2c395a086baeb35a
Yiwei Shen <YiWeiShen@users.noreply.github.com>
2024-04-06 22:47:09 -0400

    add body in html for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,18 +16,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <ConsoleMessage />
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        />
-      </head>
-      <Header />
-      <main className="bg-page-gradient pt-[var(--navigation-height)]">
-        {children}
-      </main>
-      <Footer />
+      <body>
+        <ConsoleMessage />
+        <head>
+          <meta
+            name="viewport"
+            content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          />
+        </head>
+        <Header />
+        <main className="bg-page-gradient pt-[var(--navigation-height)]">
+          {children}
+        </main>
+        <Footer />
+      </body>
     </html>
   )
 }
