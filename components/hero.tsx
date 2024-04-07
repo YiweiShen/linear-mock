094a1eed7976ab672f60355b7f4c42c11ebf53d9
Yiwei Shen <YiWeiShen@users.noreply.github.com>
2024-04-06 21:15:38 -0400

    Add className props for HeroTitle and HeroSubtitle

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,22 +1,36 @@
+import classNames from 'classnames'
+
 interface HeroProps {
+  className?: string
   children?: React.ReactNode
 }
 
 interface HeroElementProps {
+  className?: string
   children: React.ReactNode
 }
 
-export const HeroTitle = ({ children }: HeroElementProps) => {
+export const HeroTitle = ({ children, className }: HeroElementProps) => {
   return (
-    <h1 className="text-5xl mt-4 mb-12 text-gradient translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:100ms]">
+    <h1
+      className={classNames(
+        className,
+        'mt-4 mb-12 translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:100ms]'
+      )}
+    >
       {children}
     </h1>
   )
 }
 
-export const HeroSubtitle = ({ children }: HeroElementProps) => {
+export const HeroSubtitle = ({ children, className }: HeroElementProps) => {
   return (
-    <p className="mb-12 text-lg text-primary-text translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:500ms]">
+    <p
+      className={classNames(
+        className,
+        'mb-12 text-lg text-primary-text translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:500ms]'
+      )}
+    >
       {children}
     </p>
   )
